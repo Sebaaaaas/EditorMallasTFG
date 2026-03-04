81d3d0b684a7c7d2f8d512c9d5d4b3484ccd4f91
Sebaaaaas <48917257+Sebaaaaas@users.noreply.github.com>
2026-03-04 17:41:43 +0100

    Limpieza y comentarios de Mesh

--- EditorMallasTFG/Src/Mesh.h
+++ EditorMallasTFG/Src/Mesh.h
@@ -1,10 +1,7 @@
 #pragma once
 
 #include <vector>
-
 #include <string>
-
-using namespace std;
 
 class Shader;
 
@@ -13,28 +10,29 @@ struct Vertex
 {
     float Position[3];
     float Normal[3];
-    float TexCoords[2];
+    //float TexCoords[2];
 };
 
 class Mesh
 {
 private:
-    //  render data
-    unsigned int VAO, VBO, EBO;
+
+    // Mas info en https://medium.com/@deyan.sirakov2006/the-definitive-guide-to-opengl-vbos-vaos-and-ebos-6193ab13ccc5
+    unsigned int VAO, VBO, EBO; // Vertex Array Objects - Ayuda a la GPU a interpretar los valores que tiene el buffer VBO, como si fuera un manual de instrucciones para la GPU
+                                // Vertex Buffer Objects - podemos guardar un gran numero de vertices en la memoria de la GPU con toda su info(pos, colores, normales, etc)
+                                // En lugar de mandarlo continuamente desde la CPU, lo cargamos una vez en la memoria de GPU, para que lo pueda usar mas rapido
+                                // Element Buffer Objects (o Index Buffer) - Si quieres reutilizar info en lugar de repetirte, guarda indices que hacen referencia a vertices en el VBO
 
     void setupMesh();
-
 public:
-    // mesh data
-    vector<Vertex>       vertices;
-    vector<unsigned int> indices;
-    //vector<Texture>      textures;
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+    //std::vector<Texture> textures;
 
-    Mesh(vector<Vertex> vertices, vector<unsigned int> indices/*, vector<Texture> textures*/);
+    Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices/*, std::vector<Texture> textures*/);
     ~Mesh();
-    void Draw(Shader& shader);
+    void draw(Shader& shader);
 
-    static Mesh LoadOBJ(const std::string& path);
-    //void setupMesh();
+    static Mesh loadOBJ(const std::string& path);
 };
 
