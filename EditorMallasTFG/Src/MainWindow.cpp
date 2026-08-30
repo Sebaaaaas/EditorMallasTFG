eecc4e50d853d334a1b2206eb0302d0483405d0b
Sebaaaaas <48917257+Sebaaaaas@users.noreply.github.com>
2026-08-30 12:38:59 +0200

    Aplicados cambios bibliografia

--- EditorMallasTFG/Src/MainWindow.cpp
+++ EditorMallasTFG/Src/MainWindow.cpp
@@ -382,7 +382,7 @@ void MainWindow::createHelpBox(QMenuBar* menuBar) {
 			"<tr><td><br></td></tr>" "<tr><th colspan='2' align='left'>C\u00E1mara</th></tr>"
 			"<tr><td>F</td><td>&nbsp;&nbsp;Recentrar c\u00E1mara</td></tr>"
 			"<tr><td>Clic central + arrastrar</td><td>&nbsp;&nbsp;Orbitar</td></tr>"
-			"<tr><td>Shift + clic central + arrastrar</td><td>&nbsp;&nbsp;Panear</td></tr>"
+			"<tr><td>Shift + clic central + arrastrar</td><td>&nbsp;&nbsp;Desplazar</td></tr>"
 			"<tr><td>Rueda del rat\u00F3n</td><td>&nbsp;&nbsp;Zoom</td></tr>" "</table>");
 
 		helpBox.adjustSize();
